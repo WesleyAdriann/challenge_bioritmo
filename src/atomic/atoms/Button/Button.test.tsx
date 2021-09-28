0108f4aad4c11004f722d1b0fe9f5dd5f31ec752
Wesley Adriann <wesleyadriann@gmail.com>
2021-09-28 20:11:05 -0300

    Adds unit tests in button atom
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

---
+++ src/atomic/atoms/Button/Button.test.tsx
@@ -0,0 +1,50 @@
+import { render, fireEvent, cleanup} from '@testing-library/react'
+
+import { mockTestID, StorybookProviders as wrapper } from '../../../utils'
+
+import { Button, IButton } from './index'
+
+describe('Atoms/Button', () => {
+
+  const props: IButton = {
+    testID: 'Button',
+    onClick: jest.fn(),
+    text: 'Text Test'
+  }
+
+
+  const renderComponent = () => render(
+    <Button {...props} />, { wrapper }
+  )
+
+  let component = renderComponent()
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+    cleanup()
+    component = renderComponent()
+  })
+
+  const el_container = mockTestID('button', props.testID!)['data-testid']
+
+  it(`should render component #${el_container}`, async () => {
+    const sut = await component.findByTestId(el_container)
+    expect(sut).toBeTruthy()
+  })
+
+  it('should render text prop', async () => {
+    const sut = component.getByText(props.text)
+    expect(sut).toBeTruthy()
+  })
+
+  it('should call onClick prop on press in button', async () => {
+    const sut = await component.findByTestId(el_container)
+    fireEvent.click(sut)
+    expect(props.onClick).toBeCalled()
+  })
+
+  it('should render snapshot', () => {
+    expect(component.container.firstChild).toMatchSnapshot()
+  })
+
+})
